99114923a0f43870bbdc0eaa6fe5e0c7c50e22e9
magniff <tinysnippets@gmail.com>
2022-07-13 18:24:08 +0200

    Add comments.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # DMNK
 This repo presents a WIP implementation of the distributed tic-tac-toe game (distributed M,N,K game, hence the name), that suppose to run on top of the Harmony blockchain. More on that later.
+
+# Tests
+The whole build-deploy-test pipeline is wrapped with docker:
+```bash
+$ ./tests/runtest.sh
+```
--- tests/runtest.sh
+++ tests/runtest.sh
@@ -2,10 +2,15 @@
 
 export DOCKER_BUILDKIT=1
 
+# build truffle node
 docker build -t compiler -f Compiler . && \
+# build blockchain node
 docker build -t ganache -f ./tests/Blockchain ./tests && \
+# build pytest node
 docker build -t tester -f ./tests/Tester ./tests && \
+# compile and deploy contracts
 docker-compose -f ./tests/docker-compose.yml run compiler && \
+# run tests
 docker-compose -f ./tests/docker-compose.yml run tester pytest
 
 # cleanup
