ec625196863e59de010fac5a2c741bdc8591d775
magniff <tinysnippets@gmail.com>
2022-07-14 22:59:27 +0200

    Messing with tests.

--- tests/tests/conftest.py
+++ tests/tests/conftest.py
@@ -1,10 +1,9 @@
 import json
-import typing
 
 from dataclasses import dataclass
 from typing import Any, Mapping
 
-import pytest_asyncio
+import pytest
 
 from web3 import Web3
 from web3.eth import AsyncEth, Eth
@@ -19,38 +18,13 @@ GAS_LIMIT = 10 ** 7
 
 NUMBER_OF_PREPAYED_WALLETS = 2
 
-
-async def create_new_game_and_get_logs(initiator, w3, contract) -> typing.Tuple[typing.Dict[str, typing.Any]]:
-    """Calls `DMNK.new_game()` method and returns logs, embeded into the tx receipt itself.
-    """
-    return contract.events.GameCreated().processReceipt(
-        await w3.eth.wait_for_transaction_receipt(
-            await w3.eth.send_raw_transaction(
-                Eth.account.sign_transaction(
-                    await contract.functions.new_game().build_transaction(
-                        {
-                            "from": initiator.address,
-                            "chainId": CHAIN_ID,
-                            "gas": GAS_LIMIT,
-                            "gasPrice": GAS_PRICE,
-                            "nonce": await w3.eth.get_transaction_count(initiator.address),
-                            "value": 0,
-                        },
-                    ),
-                    initiator.key,
-                ).rawTransaction
-            )
-        )
-    )
-
-
 @dataclass
 class ABIAddress:
     abi: Mapping[str, Any]
     address: str
 
 
-@pytest_asyncio.fixture
+@pytest.fixture
 def dmnk_abi_address():
     with open("./build/info.json") as f:
         address = json.load(f)["address"]
@@ -59,14 +33,14 @@ def dmnk_abi_address():
     return ABIAddress(abi, address)
 
 
-@pytest_asyncio.fixture
+@pytest.fixture
 def game_abi():
     with open("./build/contracts/GameInstance.json") as f:
         return json.dumps(json.load(f)["abi"])
 
 
 
-@pytest_asyncio.fixture
+@pytest.fixture
 def w3():
     return Web3(
         Web3.AsyncHTTPProvider("http://127.0.0.1:9000"),
@@ -75,12 +49,58 @@ def w3():
     )
 
 
-@pytest_asyncio.fixture
+@pytest.fixture(scope="function")
+async def started_game(w3, dmnkContract, game_abi, prepayedWallets):
+    alice, bob, *_ = prepayedWallets
+    # Alice creates the new game
+    game_created_logs = dmnkContract.events.GameCreated().processReceipt(
+        await w3.eth.wait_for_transaction_receipt(
+            await w3.eth.send_raw_transaction(
+                Eth.account.sign_transaction(
+                    await dmnkContract.functions.new_game().build_transaction(
+                        {
+                            "from": alice.address,
+                            "chainId": CHAIN_ID,
+                            "gas": GAS_LIMIT,
+                            "gasPrice": GAS_PRICE,
+                            "nonce": await w3.eth.get_transaction_count(alice.address),
+                            "value": 0,
+                        },
+                    ),
+                    alice.key,
+                ).rawTransaction
+            )
+        )
+    )
+    game_instance = w3.eth.contract(address=game_created_logs[0]["args"]["game"], abi=game_abi)
+    # Alice and Bob join the game, first Alice and then Bob
+    for wallet in [alice, bob]:
+        await w3.eth.wait_for_transaction_receipt(
+            await w3.eth.send_raw_transaction(
+                Eth.account.sign_transaction(
+                    await game_instance.functions.join().build_transaction(
+                        {
+                            "from": wallet.address,
+                            "chainId": CHAIN_ID,
+                            "gas": GAS_LIMIT,
+                            "gasPrice": GAS_PRICE,
+                            "nonce": await w3.eth.get_transaction_count(wallet.address),
+                            "value": 10**18,
+                        },
+                    ),
+                    wallet.key,
+                ).rawTransaction
+            )
+        )
+    return alice, bob, game_instance
+
+
+@pytest.fixture
 def dmnkContract(w3, dmnk_abi_address):
     return w3.eth.contract(address=dmnk_abi_address.address, abi=dmnk_abi_address.abi)
 
 
-@pytest_asyncio.fixture
+@pytest.fixture(scope="function")
 async def prepayedWallets(w3):
     nonce = await w3.eth.get_transaction_count(DEPLOYER_PUBLIC)
     wallets = [Eth.account.create() for _ in range(NUMBER_OF_PREPAYED_WALLETS)]
@@ -88,73 +108,15 @@ async def prepayedWallets(w3):
         await w3.eth.send_raw_transaction(
             Eth.account.sign_transaction(
                 {
-                    "gas": 10 * 10 ** 6,
-                    "gasPrice": 300 * 10 ** 9,
+                    "gas": GAS_LIMIT,
+                    "gasPrice": GAS_PRICE,
                     "nonce": nonce + index,
                     "to": wallet.address,
                     "from": DEPLOYER_PUBLIC,
                     "value": 10 ** 20,
-                    "chainId": 1337,
+                    "chainId": CHAIN_ID,
                 },
                 DEPLOYER_PRIVATE,
             ).rawTransaction
         )
     return wallets
-
-
-# @pytest.fixture(scope="function")
-# def twoPlayers(w3, dmnkContract, prepayedWallets):
-#     alice, bob, *_ = prepayedWallets
-#     # Alice joins the game
-#     aliceTxHash = w3.eth.send_raw_transaction(
-#         w3.eth.account.sign_transaction(
-#             dmnkContract.functions.play(10**15, 10**16).buildTransaction(
-#                 {
-#                     "from": alice.address,
-#                     "chainId": 1666700000,
-#                     "gas": 2 * 10 ** 6,
-#                     "gasPrice": 10 ** 9,
-#                     "nonce": 0,
-#                     "value": 2 * 10 ** 15,
-#                 },
-#             ),
-#             alice.privateKey
-#         ).rawTransaction
-#     )
-#     wait_for_transaction(w3, aliceTxHash)
-
-#     create_event_found = False
-#     gameAddress = "0x0"
-#     create_event_filter = dmnkContract.events.GameCreated.createFilter(
-#         fromBlock="latest")
-#     while not create_event_found:
-#         for event in create_event_filter.get_all_entries():
-#             if event.args.alice == alice.address:
-#                 gameAddress = event.args.gameAddress
-#                 create_event_found = True
-#         time.sleep(0.2)
-
-#     # Bob joins the game
-#     bobTxHash = w3.eth.send_raw_transaction(
-#         w3.eth.account.sign_transaction(
-#             dmnkContract.functions.play(10 ** 15, 10 ** 16).buildTransaction(
-#                 {
-#                     "from": bob.address,
-#                     "chainId": 1666700000,
-#                     "gas": 2 * 10 ** 6,
-#                     "gasPrice": 10 ** 9,
-#                     "nonce": 0,
-#                     "value": 2 * 10 ** 15,
-#                 },
-#             ),
-#             bob.privateKey
-#         ).rawTransaction
-#     )
-#     wait_for_transaction(w3, bobTxHash)
-#     return (gameAddress, (alice, bob))
-
-
-# @pytest.fixture(scope="function")
-# def gameContract(w3, twoPlayers, gameInstanceABI):
-#     gameAddress, _ = twoPlayers
-#     return w3.eth.contract(address=gameAddress, abi=gameInstanceABI)
--- tests/tests/test_check.py
+++ tests/tests/test_check.py
@@ -2,11 +2,6 @@ import enum
 
 import pytest
 
-from web3.eth import Eth
-
-from .conftest import create_new_game_and_get_logs, GAS_LIMIT, GAS_PRICE, CHAIN_ID
-
-
 pytestmark = pytest.mark.asyncio
 
 
@@ -18,70 +13,80 @@ class GameStatus(enum.IntEnum):
     aborted = 4
 
 
-async def test_creat_and_join_the_game(w3, dmnkContract, prepayedWallets, game_abi):
-    alice, bob = prepayedWallets
-    transaction_logs = await create_new_game_and_get_logs(alice, w3, dmnkContract)
-    # Transaction results in a single log entry, added to the chain
-    assert len(transaction_logs) == 1
-    # This entry contains her address in the "initiator" field
-    assert transaction_logs[0]["args"]["initiator"] == alice.address
-    # Test some of the resultin game instance properties
-    game_instance = w3.eth.contract(address=transaction_logs[0]["args"]["game"], abi=game_abi)
-    # Check, that game_instance is known to the DMNK contract
-    assert await dmnkContract.functions.verify_game_address(game_instance.address).call()
-    # First move is always given to the game initiator
-    assert await game_instance.functions.get_current_player().call() == alice.address
-    # Check the initial game status
-    assert GameStatus(await game_instance.functions.get_game_status().call()) == GameStatus.created
-    # Alice joins the game
-    alice_joins_logs = game_instance.events.PlayerJoined().processReceipt(
-        await w3.eth.wait_for_transaction_receipt(
-            await w3.eth.send_raw_transaction(
-                Eth.account.sign_transaction(
-                    await game_instance.functions.join().build_transaction(
-                        {
-                            "from": alice.address,
-                            "chainId": CHAIN_ID,
-                            "gas": GAS_LIMIT,
-                            "gasPrice": GAS_PRICE,
-                            "nonce": await w3.eth.get_transaction_count(alice.address),
-                            "value": 10**18,
-                        },
-                    ),
-                    alice.key,
-                ).rawTransaction
-            )
-        )
-    )
-    # There must me only one log entry
-    assert len(alice_joins_logs) == 1
-    assert alice_joins_logs[0]["args"]["game"] == game_instance.address
-    assert alice_joins_logs[0]["args"]["player"] == alice.address
-    # Game now should be in "waiting" status
-    assert GameStatus(await game_instance.functions.get_game_status().call()) == GameStatus.waiting
-    # Bob joins the game
-    bob_joins_logs = game_instance.events.PlayerJoined().processReceipt(
-        await w3.eth.wait_for_transaction_receipt(
-            await w3.eth.send_raw_transaction(
-                Eth.account.sign_transaction(
-                    await game_instance.functions.join().build_transaction(
-                        {
-                            "from": bob.address,
-                            "chainId": CHAIN_ID,
-                            "gas": GAS_LIMIT,
-                            "gasPrice": GAS_PRICE,
-                            "nonce": await w3.eth.get_transaction_count(bob.address),
-                            "value": 10**18,
-                        },
-                    ),
-                    bob.key,
-                ).rawTransaction
-            )
-        )
-    )
-    # There must me only one log entry
-    assert len(bob_joins_logs) == 1
-    assert bob_joins_logs[0]["args"]["game"] == game_instance.address
-    assert bob_joins_logs[0]["args"]["player"] == bob.address
-    # Game now should be in "running" status
-    assert GameStatus(await game_instance.functions.get_game_status().call()) == GameStatus.running
+# async def test_creat_and_join_the_game(w3, dmnkContract, prepayedWallets, game_abi):
+#     alice, bob = prepayedWallets
+#     transaction_logs = await create_new_game_and_get_logs(alice, w3, dmnkContract)
+#     # Transaction results in a single log entry, added to the chain
+#     assert len(transaction_logs) == 1
+#     # This entry contains her address in the "initiator" field
+#     assert transaction_logs[0]["args"]["initiator"] == alice.address
+#     # Test some of the resultin game instance properties
+#     game_instance = w3.eth.contract(address=transaction_logs[0]["args"]["game"], abi=game_abi)
+#     # Check, that game_instance is known to the DMNK contract
+#     assert await dmnkContract.functions.verify_game_address(game_instance.address).call()
+#     # First move is always given to the game initiator
+#     assert await game_instance.functions.get_current_player().call() == alice.address
+#     # Check the initial game status
+#     assert GameStatus(await game_instance.functions.get_game_status().call()) == GameStatus.created
+#     # Alice joins the game
+#     alice_joins_logs = game_instance.events.PlayerJoined().processReceipt(
+#         await w3.eth.wait_for_transaction_receipt(
+#             await w3.eth.send_raw_transaction(
+#                 Eth.account.sign_transaction(
+#                     await game_instance.functions.join().build_transaction(
+#                         {
+#                             "from": alice.address,
+#                             "chainId": CHAIN_ID,
+#                             "gas": GAS_LIMIT,
+#                             "gasPrice": GAS_PRICE,
+#                             "nonce": await w3.eth.get_transaction_count(alice.address),
+#                             "value": 10**18,
+#                         },
+#                     ),
+#                     alice.key,
+#                 ).rawTransaction
+#             )
+#         )
+#     )
+#     # There must me only one log entry
+#     assert len(alice_joins_logs) == 1
+#     assert alice_joins_logs[0]["args"]["game"] == game_instance.address
+#     assert alice_joins_logs[0]["args"]["player"] == alice.address
+#     # Game now should be in "waiting" status
+#     assert GameStatus(await game_instance.functions.get_game_status().call()) == GameStatus.waiting
+#     # Bob joins the game
+#     bob_joins_logs = game_instance.events.PlayerJoined().processReceipt(
+#         await w3.eth.wait_for_transaction_receipt(
+#             await w3.eth.send_raw_transaction(
+#                 Eth.account.sign_transaction(
+#                     await game_instance.functions.join().build_transaction(
+#                         {
+#                             "from": bob.address,
+#                             "chainId": CHAIN_ID,
+#                             "gas": GAS_LIMIT,
+#                             "gasPrice": GAS_PRICE,
+#                             "nonce": await w3.eth.get_transaction_count(bob.address),
+#                             "value": 10**18,
+#                         },
+#                     ),
+#                     bob.key,
+#                 ).rawTransaction
+#             )
+#         )
+#     )
+#     # There must me only one log entry
+#     assert len(bob_joins_logs) == 1
+#     assert bob_joins_logs[0]["args"]["game"] == game_instance.address
+#     assert bob_joins_logs[0]["args"]["player"] == bob.address
+#     # Game now should be in "running" status
+#     assert GameStatus(await game_instance.functions.get_game_status().call()) == GameStatus.running
+
+
+async def test_started_game1(started_game):
+    alice, bob, game = started_game
+    assert GameStatus(await game.functions.get_game_status().call()) == GameStatus.running
+
+
+async def test_started_game0(started_game):
+    alice, bob, game = started_game
+    assert GameStatus(await game.functions.get_game_status().call()) == GameStatus.running
